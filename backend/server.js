9cf63e585e791888e3585993b81cb2f2d6c00042
sarrveshhh <sarrveshm02@gmail.com>
2023-02-11 11:48:02 +0530

    created js sever

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const request = require('request');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+    res.send('Get goals');
+})
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
