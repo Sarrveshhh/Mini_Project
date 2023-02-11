e04d5ce2768f7a54e35f6b3a9a2f428585015cf5
sarrveshhh <sarrveshm02@gmail.com>
2023-02-11 17:47:19 +0530

    completed model API and returns the output

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 import joblib
 import json
-
+import numpy as np
 
 
 app = Flask(__name__)
@@ -11,8 +11,13 @@ model = joblib.load(r'D:/Mini_Project/backend/clf_model')
 @app.route('/predict', methods=['POST'])
 def predict():
     event = json.loads(request.data)
-    print(event)
-    return "1"
+    values = event['values']
+    values = list(map(float, values))
+    pre = np.array(values)
+    pre = pre.reshape(1, -1)
+    prediction = model.predict(pre)
+
+    return str(prediction[0])
     
 
 
